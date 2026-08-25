0767aa9cb21bcb8558681d19951d508ae413d9fb
Lucas Xavier <lucasfelipe999@gmail.com>
2026-08-24 21:44:05 -0300

    Feat: Leads requests

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 // COMPONENTS
 import {
   AvatarList,
@@ -65,11 +67,11 @@ function Home() {
                 <CardComponent className={highlightsLoading ? 'skeleton-loading skeleton-loading-mh-1' : ''}>
                   {
                     !highlightsLoading && highlightsData && (
-                      <>
+                      <Link to="/leads">
                         <StyledH2 className='mb-1'>Leads contactados</StyledH2>
                         <StyledH3 className='mb-1' size={40} lineheight={40}>{highlightsData[2].value}</StyledH3>
                         <StyledSpan>{highlightsData[2].subtitle}</StyledSpan>
-                      </>
+                      </Link>
                     )
                   }
                 </CardComponent>
--- src/pages/Leads.tsx
+++ src/pages/Leads.tsx
@@ -1,10 +1,152 @@
-import { Header } from '@/components'
+// COMPONENTS
+import { useEffect, useState, type ChangeEvent } from 'react'
+import { CardComponent, CustomTable, FormComponent, Header, StyledButton, StyledSpan, StyledP, StyledH2 } from '@/components'
+import { Container, Grid } from '@mui/material'
+
+// HOOK
+import { useFormValidation, useGet, usePost, useDelete } from '@/hooks'
+
+// TYPES
+import type { InputProps, LeadsData, LeadsPostData, MessageProps } from '@/types'
 
 function Leads() {
+
+// HOOKS
+const {
+  data: createLeadsData,
+  loading: createLeadsLoading,
+  error: createLeadsError,
+  postData: createLeadsPostData,
+  } = usePost<LeadsData, LeadsPostData>('leads/create', true)
+
+const {
+  data: leadsData,
+  loading: leadsLoading,
+  error: leadsError,
+  getData: getLeads,
+} = useGet<LeadsData[]>('leads')
+
+const {
+  deleteData: leadsDeleteData,
+  loading: leadsDeleteLoading
+} = useDelete('leads/delete')
+
+// FORM
+const inputs: InputProps[] = [
+  { name: 'name', type: 'text', placeholder: 'Nome', required: true }, 
+  { name: 'email', type: 'email', placeholder: 'Email', required: true },
+  { name: 'phone', type: 'tel', placeholder: 'Telefone', required: true },
+]
+const { formValues, formValid, handleChange } = useFormValidation(inputs)
+const handleSubmit = async (e: React.FormEvent) => {
+  e.preventDefault()
+  await createLeadsPostData({
+    name: String(formValues[0]),
+    email: String(formValues[1]),
+    phone: String(formValues[2]),
+  })
+}
+
+const handleDelete = async (id: number) => {
+    if (confirm('Tem certeza que deseja excluir seu Lead?')) {
+      try {
+        await leadsDeleteData({ params: { id : id }})
+        alert('Lead deletado com sucesso!')
+        getLeads()
+      } catch (e) {
+          alert('Erro ao deletar o Lead. Entre em contato com nosso suporte.')
+        }
+    }
+  } 
+
+const [createMessage, setCreateMessage] = useState<MessageProps>({
+  type: 'success',
+  msg: ''
+})
+
+const clearMessage = () => {
+  setTimeout(() => {
+    setCreateMessage({
+      type: 'success',
+      msg: '',
+  })
+}, 3000)
+}
+
+useEffect(() => {
+  if (createLeadsData?.id) {
+    setCreateMessage({
+      msg: 'Lead criado com sucesso!',
+      type: 'success'
+    })
+    getLeads()
+    clearMessage()
+  } else if (createLeadsError) {
+    setCreateMessage({
+      msg: 'Não foi possível realizar essa operação. Entre em contato com nosso suporte.',
+      type: 'error'
+    })
+  } else clearMessage()
+}, [createLeadsData, createLeadsError])
+
   return (
     <>
       <Header />
-      <h1>Leads</h1>
+      <Container className='mb-2' maxWidth='lg'>
+        <Grid container spacing={4}>
+          <Grid size={{xs: 12, md: 7}}>
+            <CardComponent className={leadsLoading ? 'skeleton-loading skeleton-loading-mh-2' : ''}>
+              {
+                !leadsError && !leadsLoading && (
+                  <>
+                    <StyledH2>Meus Leads</StyledH2>
+                    {
+                      leadsData?.length ? (
+                        <CustomTable 
+                          headers={['Nome', 'E-mail', 'Telefone', '']}
+                          row={leadsData.map((lead) => [
+                            <StyledP>{lead.name}</StyledP>,
+                            <StyledP>{lead.email}</StyledP>,
+                            <StyledP>{lead.phone}</StyledP>,
+                            <StyledButton className='borderless-alert' onClick={() => handleDelete(lead.id)} disabled={leadsDeleteLoading}>
+                              Excluir
+                            </StyledButton>
+                          ])}
+                        />
+                      ) : <StyledSpan>Sem Leads cadastrados</StyledSpan>
+                    }
+                  </>
+                )
+              }
+            </CardComponent>
+          </Grid>
+          <Grid size={{xs: 12, md: 5}}>
+            <CardComponent>
+              <StyledH2 className='mb-1'>Cadastrar Leads</StyledH2>
+              <FormComponent
+                inputs={inputs.map((input, index) => ({
+                  ...input,
+                  type: input.type,
+                  placeholder: input.placeholder,
+                  value: formValues[index] || '',
+                  onChange: (e: ChangeEvent<HTMLInputElement>) =>
+                    handleChange(index, (e.target as HTMLInputElement).value),
+                }))}
+                buttons={[
+                  {
+                    className: 'primary',
+                    disabled: !formValid || createLeadsLoading || leadsDeleteLoading,
+                    type: 'submit',
+                    onClick: handleSubmit,
+                    children: 'Cadastrar Lead',
+                  }
+                ]}
+                message={createMessage}
+              />
+            </CardComponent>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   )
 }
